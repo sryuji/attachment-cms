697d34e934a839b12a2ec65e36be7bf68573f5c1
Ryuji Sakon <promi0904@gmail.com>
2021-10-31 17:25:00 +0900

    feat: ブラウザ側でconfigurationだけheadに定義し、libをロードすればrunするように対応

--- attachment/src/main.ts
+++ attachment/src/main.ts
@@ -1,3 +1,2 @@
 import { AttachmentCMS } from './attachment-cms'
-
-new AttachmentCMS('f6974698-a322-4e89-b32c-7f4ab5808fc9', 'http://localhost:3000').run()
+new AttachmentCMS(AttachmentConfig.token, AttachmentConfig.url).run()
